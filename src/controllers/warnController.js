9011d5b3e72ada2787824060b6a4c60c6a7abe17
www-data <www-data@srvinet2.imobiliar.com.br>
2024-02-09 14:43:27 -0300

    Mudanca de visual e adicionado o teste de conexao com o cliente no painel

--- src/controllers/warnController.js
+++ src/controllers/warnController.js
@@ -1,7 +1,65 @@
+const fs = require('fs');
+const { Socket } = require('net');
+const { basename } = require('path');
 const { execSync } = require("child_process");
+const { promisify } = require('util');
+const execFile = promisify(require('child_process').execFile);
 
 const { unlinkSync, writeFileSync } = require('fs');
 const { getNextModbase, setOKResponse, setErrorResponse } = require('../utils')
+
+function testHostPortAccessibility(host, port, response) {
+	const socket = new Socket();
+
+	socket.setTimeout(5000); // Set a timeout in milliseconds
+
+	socket.on('connect', () => {
+		const msg = `Sucesso ao conectar com o cliente ${host}:${port}`;
+		console.log(msg);
+		socket.destroy();
+		response.json(setOKResponse(msg));
+	});
+
+	socket.on('timeout', () => {
+		const msg = `Tempo de conexao com o cliente ${host}:${port} expirou. \nTente novamente mais tarde.`;
+		console.error(msg);
+		socket.destroy();
+		response.json(setErrorResponse(msg));
+	});
+
+	socket.on('error', (error) => {
+		const msg = `Erro ao conectar com o cliente ${host}:${port}: ${error.message}`;
+		console.error(msg);
+		socket.destroy();
+		response.json(setErrorResponse(msg));
+	});
+
+	socket.on('close', (hadError) => {
+		if (hadError) {
+			console.error(`Socket closed due to errors`);
+		} else {
+			console.log(`Socket closed gracefully`);
+		}
+	});
+
+	socket.connect(+port, host);
+}
+
+async function execScript(command, arguments) {
+	let success = false;
+
+	try {
+		output = await execFile(command, arguments);
+		if (!output.stderr) {
+			success = true;
+			console.log('success', output.stdout);
+		}
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+	}
+
+	return success;
+}
 
 const validate = (texto, dateIni, dateEnd) => {
 	if (!texto) {
@@ -24,7 +82,7 @@ const validate = (texto, dateIni, dateEnd) => {
 }
 
 module.exports = {
-	create: (req, res) => {
+	create: async (req, res) => {
 		// Valida parametros da requisicao
 		let { texto, dateIni, dateEnd } = req.body.data;
 
@@ -54,7 +112,7 @@ module.exports = {
 
 			writeFileSync(fullPathNextModbase, textoSql);
 
-			fullPathNextModbaseDest = `${process.env.DIR_MODBASE}/mod_base_sql.${nextModbase.split('.')[1]}.prd.tst`;
+			fullPathNextModbaseDest = `${process.env.DIR_MODBASE}mod_base_sql.${nextModbase.split('.')[1]}.prd.tst`;
 			execSync(`iconv ${fullPathNextModbase} -futf8 -tiso88591 > ${fullPathNextModbaseDest}`);
 			unlinkSync(fullPathNextModbase);
 		} catch (error) {
@@ -62,16 +120,27 @@ module.exports = {
 		}
 
 		try {
-			execSync(`${process.env.DIR_MODBASE}/copiabase.sh ${fullPathNextModbaseDest}`);
+			const currentDir = process.cwd();
+			process.chdir(process.env.DIR_MODBASE);
+
+			await execScript(`/home/svn/repositorio.sh`, [`unlock`]);
+			await execScript(`./copiabase.sh`, [basename(`${fullPathNextModbaseDest}`)]);
+			await execScript(`/home/svn/repositorio.sh`, [`lock`]);
+
+			process.chdir(currentDir);
 		} catch (error) {
-			const messageError = 
-				`Modbase ${fullPathNextModbaseDest} de lembrete criado no srvinet2 com sucesso, `+
-				`mas não foi possível publicar o mesmo com o programa copiabase.\n`+
-				`Favor pedir para algum desenvolvedor efetuar o copia base!`;
+			const messageError =
+				`Modbase ${fullPathNextModbaseDest} de lembrete criado no srvinet2 com sucesso, ` +
+				`mas não foi possível publicar o mesmo com o programa copiabase.\n` +
+				`Favor pedir para algum desenvolvedor efetuar o copia base!\n` +
+				`Error message: ${error}`;
 			return res.json(setErrorResponse(messageError));
-		} 
+		}
 
 		return res.json(setOKResponse('Modbase criado e publicado com sucesso!'));
-
+	},
+	connect: async (req, res) => {
+		const { host, port } = req.body;
+		testHostPortAccessibility(host, new Number(port), res);
 	}
 }
--- src/routes/warnRoutes.js
+++ src/routes/warnRoutes.js
@@ -3,5 +3,6 @@ const { logRequest } = require('../utils')
 const WarnController = require('../controllers/warnController');
 
 router.post("/warn", logRequest, WarnController.create)
+router.post("/connect", logRequest, WarnController.connect)
 
 module.exports = router;
